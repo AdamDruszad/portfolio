138ee15d5c38d77851f520eebcf22354a6319bad
AdamDruszad <adambiro2008@gmail.com>
2026-06-11 11:11:56 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import About from "./components/About";
 
 import { Analytics } from "@vercel/analytics/react";
 
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import { ReactLenis } from "lenis/react";
 
