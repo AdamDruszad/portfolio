b8a338d36a994802539d4c2a763f1a34e0abe99f
AdamDruszad <adambiro2008@gmail.com>
2026-07-16 23:02:37 +0200

    Change one project link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -67,7 +67,7 @@ const projects = [
     description: "A Blender project, for submission",
     tech: "Blender",
     status: "in progress",
-    link: "#",
+    link: "https://sketchfab.com/3d-models/m4--competition-8a10fc22e7a64e6197a4625728575ccd",
   },
 ];
 
